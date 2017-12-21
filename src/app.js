f15bc41f68262ffe0e0c7a2bffe1ce48bcd3ba23
ZuzaB <lacznaz@wp.pl>
2017-12-21 16:38:13 +0100

    add cookie value dysplaying

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
-
-
-//funkcja do obliczania nowej ceny
-// var priceNew = function newPrice(price, counter) {
-//   priceNew = price + counter * 0.1 * price;
-//   return priceNew;
-// }
 let cookieVal = 0,
-    cursorCounter = 0,
-    grandmaCounter = 0,
-    farmCounter = 0,
-    mineCounter = 0,
-    factoryCounter = 0,
-    priceNew = (price,counter) => price + counter * 0.1 * price, //funkcja do obliczania nowej ceny
-    production = (prodVal, counter) => prodVal * counter;//oblicznie produkcji jednego producenta w 1 sec
+  cursorCounter = 0,
+  grandmaCounter = 0,
+  farmCounter = 0,
+  mineCounter = 0,
+  factoryCounter = 0,
+  priceNew = (price, counter) => price + counter * 0.1 * price, //funkcja do obliczania nowej ceny
+  production = (prodVal, counter) => prodVal * counter, //oblicznie produkcji jednego producenta w 1 sec
+  cursorProdVal = production(0.1, cursorCounter),
+  grandmaProdVal = production(1, grandmaCounter),
+  farmProdVal = production(8, farmCounter),
+  mineProdVal = production(47, mineCounter),
+  factoryProdVal = production(260, factoryCounter),
+  cookieProdSec = cursorProdVal + grandmaProdVal + farmProdVal + mineProdVal + factoryProdVal; //oblicznie produkcji na sec
 
-let cursorProdVal = production(0.1, cursorCounter),
- grandmaProdVal = production(1, grandmaCounter),
- farmProdVal = production(8, farmCounter),
- mineProdVal = production(47, mineCounter),
- factoryProdVal = production(260, factoryCounter),
- cookieProdSec = cursorProdVal + grandmaProdVal + farmProdVal + mineProdVal + factoryProdVal;//oblicznie produkcji na sec
-setInterval(()=> console.log (Math.floor(cookieVal+=cookieProdSec)), 1000);
+const cookieValDiv = document.querySelector('.cookie-quantity'),
+  cookieValDivSec = document.querySelector('.cookie-quantity-sec');
 
-console.log ();
+setInterval(function() {
+
+  let currCookieVal = Math.floor(cookieVal += cookieProdSec); //obecny stan ciasteczek
+
+  cookieValDiv.innerText = currCookieVal + ' cookies';
+}, 1000);
+
+cookieValDivSec.innerText = 'per second: ' + cookieProdSec;
